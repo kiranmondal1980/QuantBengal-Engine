ec450e79a0b64d53afab49478a37b18a0c6872a8
kiranmondal1980 <118626526+kiranmondal1980@users.noreply.github.com>
2026-04-21 13:57:09 +0300

    Update broker_api.py

--- broker_api.py
+++ broker_api.py
@@ -1,8 +1,26 @@
+import os
 import logging
+from SmartApi.smartConnect import SmartConnect
+import pyotp
 
 class IndianBrokerAPI:
-    def execute_order(self, signal):
-        # This is where you put your AngelOne order logic
-        logging.info(f"🚨 EXECUTING {signal} BASED ON TRADINGVIEW ALERT 🚨")
-        # Add your AngelOne SmartAPI 'placeOrder' code here
-        return True
+    def __init__(self):
+        self.api_key = os.environ.get("BROKER_API_KEY")
+        self.client_id = os.environ.get("CLIENT_ID")
+        self.password = os.environ.get("PASSWORD")
+        self.token = os.environ.get("TOTP_TOKEN")
+        self.obj = SmartConnect(api_key=self.api_key)
+        
+        totp = pyotp.TOTP(self.token).now()
+        self.obj.generateSession(self.client_id, self.password, totp)
+        logging.info("Session generated.")
+
+    def get_data(self):
+        # 35002 is BankNifty Index
+        params = {"exchange": "NSE", "symboltoken": "35002", "interval": "FIFTEEN_MINUTE", 
+                  "fromdate": "2024-05-20 09:15", "todate": "2024-05-21 15:30"}
+        return self.obj.getCandleData(params)['data']
+
+    def place_order(self, signal):
+        logging.info(f"🚨 EXECUTING TRADE: {signal} 🚨")
+        # Add actual order placement logic here later
