68ceb4bf2f20628aded6ceef72b76fc0eeb4a2f3
kiranmondal1980 <118626526+kiranmondal1980@users.noreply.github.com>
2026-04-21 16:48:21 +0300

    Update broker_api.py

--- broker_api.py
+++ broker_api.py
@@ -39,7 +39,7 @@ class IndianBrokerAPI:
         params = {
             "exchange": "NSE",
             "symboltoken": "99926009", 
-            "interval": "FIVE_MINUTE", 
+            "interval": "FIFTEEN_MINUTE", 
             "fromdate": from_date_str, 
             "todate": to_date_str
         }
